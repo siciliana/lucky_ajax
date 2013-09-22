0cd7dbf26a7571ff6dc099e8980ff27876b14cad
Apprentice <apprentice@dbc-chicago-009.local>
2013-09-22 17:31:36 -0500

    update previous commit

--- public/js/application.js
+++ public/js/application.js
@@ -1,10 +1,23 @@
-$(document).ready(function () {
+$(document).ready(function() {
+    $('#roll').click(function(event){
+        //   1- intercept the form submission event using jQuery
+        //   2- prevent the default action for that event from happening
+        event.preventDefault();
+        //   3- generate a random number between 1 and 6 using JavaScript
+        var roll = Math.floor(Math.random() * 6) + 1
+        //   4- use jQuery to submit an AJAX post to the form's action
+        $.post('/rolls', function(data){
+                $('#die').html(data);
+                console.log(data);
+
+        });
+               
+    });
+});
 
   // PSEUDO-CODE:
-  //   1- intercept the form submission event using jQuery
-  //   2- prevent the default action for that event from happening
-  //   3- generate a random number between 1 and 6 using JavaScript
-  //   4- use jQuery to submit an AJAX post to the form's action
+
+
   //   5- when the AJAX post is done, replace the contents of the "#die" DIV in the DOM using jQuery
 
-});
+
